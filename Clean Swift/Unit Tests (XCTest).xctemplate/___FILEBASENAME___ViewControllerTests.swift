dc6dc6d149db1cdcbee580906cde8092db5fea9a
Zaid M. Said <zaid.shr@gmail.com>
2019-07-05 16:27:47 +0800

    Rename project name to package name for test templates

--- Clean Swift/Unit Tests (XCTest).xctemplate/___FILEBASENAME___ViewControllerTests.swift
+++ Clean Swift/Unit Tests (XCTest).xctemplate/___FILEBASENAME___ViewControllerTests.swift
@@ -1,6 +1,6 @@
 //
 //  ___FILENAME___
-//  ___PROJECTNAME___
+//  ___PACKAGENAME___
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
